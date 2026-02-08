6a8537b39063186503fc0f632a4e6ca6610e130c
sjshin001 <shise@nate.com>
2026-02-08 10:59:10 +0900

    Update validPassword default value to an empty string

--- config/kurly.config.ts
+++ config/kurly.config.ts
@@ -3,7 +3,7 @@ export const kurlyConfig = {
 
   testData: {
     validId: process.env.TEST_ID || '',
-    validPassword: process.env.TEST_PASSWORD || 'p',
+    validPassword: process.env.TEST_PASSWORD || '',
     kurlyPayPassword: process.env.KURLY_PAY_PASSWORD || '',
     paycoPin: process.env.PAYCO_PIN || '832013', // 페이코 결제 비밀번호
   },
