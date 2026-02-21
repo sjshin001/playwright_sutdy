d26093256bd93afe83fae11fff6f84e59f80d006
sjshin001 <shise@nate.com>
2026-02-22 00:33:02 +0900

    Remove default PAYCO_PIN value from config

--- config/kurly.config.ts
+++ config/kurly.config.ts
@@ -5,7 +5,7 @@ export const kurlyConfig = {
     validId: process.env.TEST_ID || '',
     validPassword: process.env.TEST_PASSWORD || '',
     kurlyPayPassword: process.env.KURLY_PAY_PASSWORD || '',
-    paycoPin: process.env.PAYCO_PIN || '832013', // 페이코 결제 비밀번호
+    paycoPin: process.env.PAYCO_PIN || '', // 페이코 결제 비밀번호
   },
 
   waitTimes: {
